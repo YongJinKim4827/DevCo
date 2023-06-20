d2b1c5d9f30b39c1909350a102abe87e55e8d655
sccs604@naver.com <sccs604@naver.com>
2023-06-20 16:28:52 +0900

    회원가입 화면 틀 생성

---
+++ front-end/src/signup/SignUp.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import "./signup.css"
+
+const SignUp = () =>  {
+    const [signUpInfo, setSignUpInfo] = useState({
+        userId : '',
+        userPassword : '',
+        reEnterPassword : '',
+        checkPassword : false,
+        userName : '',
+        gender : '',
+        email : ''
+    });
+
+    const onChange = (event) => {
+        if(event.target.id === "signUpIdInput"){
+            setSignUpInfo({...signUpInfo, userId : event.target.value});
+        }else if(event.target.id === "signUpPassword"){
+            setSignUpInfo({...signUpInfo, userPassword : event.target.value});
+        }else if(event.target.id === "signUpReEnterPassword"){
+            setSignUpInfo({...signUpInfo, reEnterPassword : event.target.value});
+        }else if(event.target.id === "signUpName"){
+            setSignUpInfo({...signUpInfo, userName : event.target.value});
+        }else if(event.target.id === "signUpIdGender"){
+            setSignUpInfo({...signUpInfo, gender : event.target.value});
+        }else if(event.target.id === "signUpEmail"){
+            setSignUpInfo({...signUpInfo, email : event.target.value});
+        }
+    }
+
+    useEffect(()=>{//비밀번호 체크 동기 처리
+        if(signUpInfo.userPassword === signUpInfo.reEnterPassword){
+            setSignUpInfo({...signUpInfo, checkPassword : true});
+        }
+    },[
+        signUpInfo.reEnterPassword, 
+        signUpInfo.userPassword
+        ]);
+
+    const SignUpHandler = (event) => {
+        event.preventDefault();
+        console.dir(signUpInfo);
+        axios.post("/signup")
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+    }
+
+    const moveLoginPage = () => {
+        window.location.replace("/");
+    }
+
+    return (
+        <div className='signUpContainer'>
+            <form action="" method="POST" onSubmit={SignUpHandler} className='signUpForm'>
+                <div className='content'>
+                <div className="mb-3">
+                    <label htmlFor="signUpIdInput" className="form-label">ID</label>
+                    <input type="text" className="form-control" 
+                        id="signUpIdInput"
+                        value={signUpInfo.userId} onChange={onChange}
+                        />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor= "signUpPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control" 
+                        id="signUpPassword" value={signUpInfo.userPassword} onChange={onChange}
+                        />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="signUpReEnterPassword" className="form-label">비밀번호 재확인</label>
+                    <input type="password" className="form-control" 
+                        id="signUpReEnterPassword"
+                        value={signUpInfo.reEnterPassword} onChange={onChange}
+                        />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor= "signUpName" className="form-label">이름</label>
+                    <input type="text" className="form-control" 
+                        id="signUpName" value={signUpInfo.userName} onChange={onChange}
+                        />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="signUpIdGender" className="form-label">성별</label>
+                    <input type="text" className="form-control" 
+                        id="signUpIdGender"
+                        value={signUpInfo.gender} onChange={onChange}
+                        />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor= "signUpEmail" className="form-label">본인확인 이메일</label>
+                    <input type="email" className="form-control" 
+                        id="signUpEmail" value={signUpInfo.email} onChange={onChange}
+                        />
+                </div>
+                {/* <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor= "exampleCheck1">Check me out</label>
+                </div> */}
+                </div>
+                <div>
+                    <button type='button' className="btn btn-primary" onClick={moveLoginPage}>{"< Prev"}</button>
+                    <button type='submit' className="btn btn-primary" >Sign Up</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp
