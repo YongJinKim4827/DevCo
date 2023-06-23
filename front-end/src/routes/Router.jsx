24af5251128926af52986617fc1add69e0659521
sccs604@naver.com <sccs604@naver.com>
2023-06-23 16:08:45 +0900

    화면 라우팅

--- front-end/src/routes/Router.jsx
+++ front-end/src/routes/Router.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from "../login/Login";
 import SignUp from "../signup/SignUp";
+import Layout from '../main/Layout';
+import Main from '../main/Main';
+
 
 const Router = () => {
   return (
     <>
+
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/signup" element={<SignUp/>}/>
-                {/* <Route path="/memo" element={<Login/>}/> */}
+              <Route>
+                  <Route path="/*" element={<Layout/>}/>
+                  <Route path="/login" element={<Login/>}/>
+                  <Route path="/signup" element={<SignUp/>}/>
+              </Route>
             </Routes>
         </BrowserRouter>
     </>
