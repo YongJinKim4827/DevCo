e9f4103dbf4adb888a437bfca89284c16c17c667
sccs604@naver.com <sccs604@naver.com>
2023-06-28 13:15:17 +0900

    Mybatis 설정

--- back-end/src/main/java/com/cyberi/devcommunity/config/MybatisConfig.java
+++ back-end/src/main/java/com/cyberi/devcommunity/config/MybatisConfig.java
@@ -1,10 +1,9 @@
-package com.cyberi.config;
+package com.cyberi.devcommunity.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
