55abaa680a351ef8620884f320e6e8eaf7848969
sccs604@naver.com <sccs604@naver.com>
2023-06-29 17:24:24 +0900

    Config 및 apllication.propertis 보완

--- back-end/src/main/java/com/cyberi/devcommunity/config/SpringConfig.java
+++ back-end/src/main/java/com/cyberi/devcommunity/config/SpringConfig.java
@@ -1,15 +1,23 @@
 package com.cyberi.devcommunity.config;
 
+import com.cyberi.devcommunity.repository.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.cyberi.devcommunity.repository.UserRepository;
-import com.cyberi.devcommunity.repository.UserRepositoryImpl;
 
 @Configuration
 public class SpringConfig {
     @Bean
     public UserRepository userRepository(){
         return new UserRepositoryImpl();
+    }
+
+    @Bean
+    public BoardRepository boardRepository(){
+        return new BoardRepositoryImpl();
+    }
+
+    @Bean
+    public ReplyRepository replyRepository(){
+        return new ReplyRepositoryImpl();
     }
 }
