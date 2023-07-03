39ea350d5c95ee5284d815b94b8574b53a2e74e3
sccs604@naver.com <sccs604@naver.com>
2023-07-03 17:21:07 +0900

    Header 부분 UI 및 화면 라우팅

--- front-end/src/common/Header.jsx
+++ front-end/src/common/Header.jsx
@@ -3,16 +3,29 @@ import './common.css'
 import logoImg from '../assets/img/uncle-sam.png';
 import messageImg from '../assets/img/free-icon-chat-2450503.png';
 import userImg from '../assets/img/free-icon-default-user.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigation = useNavigate();
   const moveChattingPage = () => {
-    console.log("Move Chatting Page...")
+    navigation("/chat");
   }
 
+  const moveUserInfoPage = () => {
+    navigation("/user");
+  }
+
+  const moveAccountInfoPage = () => {
+    navigation("/user/account");
+  }
+
+  const moveMainPage = () => {
+    navigation("/");
+  }
   
   return (
-    <div className='div-header-wrapper'>
-      <div style={{display : "flex"}}>
+    <div className='div-header-wrapper' style={{flex : "0.1"}}>
+      <div className='div-header-logo' onClick={moveMainPage}>
         <img src={logoImg}
                         style={{width : "80px", height : "80px", marginBottom : "3px"}}
         />
@@ -37,13 +50,18 @@ const Header = () => {
       </div>
       <div style={{display : "flex"}}>
         <div style={{display : "inline", alignSelf : "center", position : "relative"}} >
-            <img src={messageImg} className='img-header' />
-            <div className='div-chat-alarm'>
+            <img src={messageImg} className='img-header' onClick={moveChattingPage}/>
+            <div className='div-chat-alarm' onClick={moveChattingPage}>
               <span>+5</span>
             </div>
         </div>
         <div style={{display : "inline", alignSelf : "center", marginLeft : "20px"}}>
-          <img src={userImg} className='img-header'/>
+          <img src={userImg} className='img-header'data-bs-toggle="dropdown" aria-expanded="false"/>
+          <ul className="dropdown-menu">
+            <li><button className='dropdown-item' style={{borderRadius : '0px'}} onClick={moveAccountInfoPage}>내 계정</button></li>
+            <li><button className='dropdown-item' style={{borderRadius : '0px'}} onClick={moveUserInfoPage}>내 정보</button></li>
+            <li><button className='dropdown-item' style={{borderRadius : '0px'}}>로그아웃</button></li>
+          </ul>
         </div>
       </div>
     </div>
