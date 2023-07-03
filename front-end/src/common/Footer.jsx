4d38e15048c113389b76f4c7e0a0935422895489
sccs604@naver.com <sccs604@naver.com>
2023-07-03 17:22:31 +0900

    Footer 영역 분할

--- front-end/src/common/Footer.jsx
+++ front-end/src/common/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import logoImg from '../assets/img/uncle-sam.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div style={{height : "100px", borderTop : '1px solid black', display : "flex", justifyContent:"center", flex : "0.1"}}>
+      <div style={{display : "flex", justifyContent:"space-around", width : "80vw", maxWidth : "1500px", marginTop : "10px"}}>
+        <div>
+          <div className='div-header-logo'>
+            <img src={logoImg}
+                            style={{width : "80px", height : "80px", marginBottom : "3px"}}
+            />
+            <div style={{display : "inline", alignSelf : "center", marginLeft : "10px"}}>
+              <h1 style={{fontWeight : "bold"}}>DEVCO</h1>
+            </div>
+          </div>
+        </div>
+        <div> 
+          <span>김용진</span>
+        </div>
+        <div>
+          회사
+        </div>
+      </div>
+    </div>
   )
 }
 
