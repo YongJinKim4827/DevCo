afe0bc3be835e85b6e887c9990b606abc5cc7a7e
sccs604@naver.com <sccs604@naver.com>
2023-07-03 17:29:25 +0900

    조회수 증가 기능 구현

--- back-end/src/main/java/com/cyberi/devcommunity/repository/BoardRepository.java
+++ back-end/src/main/java/com/cyberi/devcommunity/repository/BoardRepository.java
@@ -8,7 +8,7 @@ public interface BoardRepository {
     public List<BoardItem> selectBoardItems(BoardItem boardItem);
     public BoardItem selectBoardItem(BoardItem boardItem);
     public int updateBoardItem(BoardItem boardItem);
-    public int increaseViews();
+    public int increaseViews(BoardItem boardItem);
     public int registryBoardItem(BoardItem boardItem);
     public int deleteBoardItem(BoardItem boardItem);
 }
