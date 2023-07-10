4604c1d0fefb1b4a3755233ea6de7f85ca89900c
sccs604@naver.com <sccs604@naver.com>
2023-07-10 17:14:29 +0900

    채팅 기능 보완

--- back-end/src/main/java/com/cyberi/devcommunity/dto/ChatMessageItem.java
+++ back-end/src/main/java/com/cyberi/devcommunity/dto/ChatMessageItem.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ChatMessageItem {
-    private Integer roomSeq;
-    private String message;
-    private String user;
+    private String chatContent;
+    private String chattingRoomNo;
+    private int chatContentNo;
+    private String readed;
+    private Date chattingDate;
+    private String userId;
 }
