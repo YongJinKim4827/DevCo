08f2fc5b1db3bd1067403eaa62c2ada59432c871
YongJinKim4827 <sccs604@naver.com>
2023-07-11 01:24:58 +0900

    관리자 사용자 관리화면 구현

--- front-end/src/user/UserLeftNav.jsx
+++ front-end/src/user/UserLeftNav.jsx
@@ -13,9 +13,7 @@ const UserLeftNav = () => {
               <button onClick={(event) => {navigation("/user/account")}}>계정</button>
               <button onClick={(event) => {navigation("/user/activity")}}>활동이력</button>
               <button onClick={(event) => {navigation("/user/myboard")}}>나의 게시글</button>
-          </div>
-          <div>
-
+              <button onClick={(event) => {navigation("/user/admin")}}>사용자 관리</button>
           </div>
         </>
         
---
+++ front-end/src/user/UserManage.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import UserManageItem from './UserManageItem';
+import { getCookie } from '../login/Cookies';
+const UserManage = () => {
+    const [userItems, setUserItems] = useState([]);
+    useEffect(()=> {
+        debugger;
+        axios.get(`${REQUEST_ORIGIN}/admin/user/select`,{
+            headers : {
+                Authorization : `Bearer ${getCookie("token").accessToken}`
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            setUserItems(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    },[])
+
+    const saveUserItem = (item) => {
+
+        axios.post(`${REQUEST_ORIGIN}/admin/user/update`,
+        item
+        )
+        .then((res) => {
+            debugger;
+        })
+        .catch((err) => {
+
+        })
+    }
+    return(
+        <div style={{width : "50vw", marginTop : '25px'}}>
+            <div style={{display : "flex", padding : "10px", justifyContent : "space-between", borderBottom : "1px solid #DCDEE1"}}>
+                <div style={{width : "20%"}}>
+                    <h5 style={{fontWeight : "bold"}}>User ID</h5>
+                </div>
+                <div style={{width : "20%"}}> <h5 style={{fontWeight : "bold"}}>ROLE</h5></div>
+                <div style={{width : "20%"}}> <h5 style={{fontWeight : "bold"}}>Valid</h5></div>
+                <div style={{width : "20%"}}> <h5 style={{fontWeight : "bold"}}>Save</h5></div>
+            </div>
+            <div>
+                {
+                    userItems.length > 0 ?
+                    userItems.map((item, idx) => {
+                        return (<UserManageItem key={`userItem_${idx+1}`} item = {item} save = {saveUserItem}/>)
+                    })
+                    : 
+                    ''
+                }
+            </div>
+        </div>
+    )
+}
+
+export default UserManage
