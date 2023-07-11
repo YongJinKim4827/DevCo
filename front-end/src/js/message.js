f88a4383654535cd26bf2739be6b89f7495bd61b
sccs604@naver.com <sccs604@naver.com>
2023-07-11 17:08:38 +0900

    Welcome 컴포넌트 로고 제거 및 공통 메시지/상수 선언

---
+++ front-end/src/js/message.js
@@ -0,0 +1,5 @@
+const PLEASE_LOGIN_MSG = "로그인 후 이용해주세요."
+const NO_SHARE_MSG = "해당 글을 공유되지 않아 다른 사용자들이 볼 수 없습니다. 저장하시겠습니까?"
+const LOGIN_FAIL = "로그인에 실패하였습니다."
+const SHARE_MSG = "해당 글을 공유되지 않아 다른 사용자들이 볼 수 있습니다. 저장하시겠습니까?"
+const PLEASE_INPUT_EMAIL_MSG = "E-MAIL을 입력해주세요"
